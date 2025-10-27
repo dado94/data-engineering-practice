c7dfbcbbddbff869ce930112468569212adbe922
davidebaitelli <davide.baitelli94@gmail.com>
2025-10-28 00:00:39 +0100

    exercise 6 not finished

--- Exercises/Exercise-6/main.py
+++ Exercises/Exercise-6/main.py
@@ -18,8 +18,20 @@ def main():
 
     # loading in spark
     spark = SparkSession.builder.appName("Exercise6").enableHiveSupport().getOrCreate()
-    df = spark.read.csv(f'{os.path.join(file_folder_path, file_list[0])}', header=True, inferSchema=True)
-    df.orderBy('start_time', ascending=False).show()
+    df_2019 = spark.read.csv(f'{os.path.join(file_folder_path, file_list[0])}', header=True, inferSchema=True)
+    df_2020 = spark.read.csv(f'{os.path.join(file_folder_path, file_list[1])}', header=True, inferSchema=True)
+
+    # data preparation 2020 > 2019 (which has more fields)
+    columns_rename = {
+        
+    }
+    df_2020.withColumnRenamed("ride_id", "trip_id")
+    df_2020.withColumnRenamed("started_at", "start_time")
+    df_2020.withColumnRenamed("ended_at", "end_time")
+
+
+    df_total = df_2019.unionByName(df_2020)
+    # df.orderBy('start_time', ascending=False).show()
 
     # query_1 = ' \
     #     SELECT avg(end_time - start_time) \
@@ -27,20 +39,8 @@ def main():
     #     GROUP BY \
     # '
 
-    # trip_id,start_time,end_time,bikeid,tripduration,from_station_id,from_station_name,to_station_id,to_station_name,usertype,gender,birthyear
-    # ride_id,rideable_type,started_at,ended_at,start_station_name,start_station_id,end_station_name,end_station_id,start_lat,start_lng,end_lat,end_lng,member_casual
-    # trip_id > ride_id (+-)
-    # start_time > started_at
-    # end_time > ended_at
-    # bikeid ?
-    # tripduration
-    # from_station_id > start_station_id
-    # from_station_name > start_station_name
-    # to_station_id > end_station_id
-    # to_station_name > end_station_name
-    # usertype > ? member_casual
-    # gender ? 
-    # birthyear ? 
-
+    # 2019: trip_id,start_time,end_time,bikeid,tripduration,from_station_id,from_station_name,to_station_id,to_station_name,usertype,gender,birthyear
+    # 2020: ride_id,rideable_type,started_at,ended_at,start_station_name,start_station_id,end_station_name,end_station_id,start_lat,start_lng,end_lat,end_lng,member_casual
+    
 if __name__ == "__main__":
     main()
